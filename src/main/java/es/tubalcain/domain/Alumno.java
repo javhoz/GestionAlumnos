bb1b7907b14b57ffa12a9898b9cec08d40e80828
javhoz <fjavhoz@gmail.com>
2025-11-06 08:30:24 +0100

    added imports

--- src/main/java/es/tubalcain/domain/Alumno.java
+++ src/main/java/es/tubalcain/domain/Alumno.java
@@ -1,16 +1,20 @@
 package es.tubalcain.domain;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Alumno")
