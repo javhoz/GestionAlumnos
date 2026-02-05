7eb16546072cebd78396902488fd46203e83294c
javhoz <fjavhoz@gmail.com>
2026-02-05 11:57:16 +0100

    removed vaadin junk and modified pom for deployment

--- src/main/java/es/tubalcain/GestionAlumnosApplication.java
+++ src/main/java/es/tubalcain/GestionAlumnosApplication.java
@@ -3,12 +3,19 @@ package es.tubalcain;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication
-public class GestionAlumnosApplication {
+public class GestionAlumnosApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GestionAlumnosApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GestionAlumnosApplication.class, args);
